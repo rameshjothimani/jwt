f5877df68a8f939fa4d07f9f56e830698e6741ad
Ramesh Jothimani <ramesh.iit19@gmail.com>
2018-03-05 17:07:56 +0100

    Keypair (Private & Public keys) read from JCK file

--- Signature.java
+++ Signature.java
@@ -1,86 +1,115 @@
 package com.webtoken.sign;
 
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
-import java.security.NoSuchAlgorithmException;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.*;
+import java.security.Certificate;
+import java.security.cert.*;
 import java.security.interfaces.*;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
 import java.text.ParseException;
 import javax.crypto.*;
 
+import com.ibm.crypto.pkcs11impl.provider.*;
 import com.nimbusds.jose.*;
 import com.nimbusds.jose.crypto.*;
 
 public class Signature {
 
-public static void main(String args[]) {
+    public static void main(String args[]) {
+
+        String keystorePass="password";
+        String keyPass="password";
+
+        String alias="securitytest";
 
 
-    // RSA signatures require a public and private RSA key pair,
-// the public key must be made known to the JWS recipient to
-// allow the signatures to be verified
-    KeyPairGenerator keyGenerator = null;
-    try {
-        keyGenerator = KeyPairGenerator. getInstance("RSA");
-    } catch (NoSuchAlgorithmException e) {
-        e.printStackTrace();
+        Key key=null;
+        java.security.cert.Certificate cert=null;
+
+        try {
+
+            InputStream keystoreStream = new FileInputStream("C://UserTemp//keystore//tpp.jck");
+            KeyStore keystore = KeyStore.getInstance("JCEKS");
+            keystore.load(keystoreStream, keystorePass.toCharArray());
+
+        if (!keystore.containsAlias(alias)) {
+            throw new RuntimeException("Alias for key not found");
+        }
+        key = keystore.getKey(alias, keyPass.toCharArray());
+
+        cert = keystore.getCertificate(alias);
+
+        } catch (FileNotFoundException filenotfoundexcep){
+            filenotfoundexcep.printStackTrace();
+        }catch(KeyStoreException keystoreException){
+            keystoreException.printStackTrace();
+        }catch(CertificateException certificateException){
+            certificateException.printStackTrace();
+        }catch(IOException ioException){
+            ioException.printStackTrace();
+        }catch(NoSuchAlgorithmException nosuchAlgorithmException){
+            nosuchAlgorithmException.printStackTrace();
+        }catch(UnrecoverableKeyException unrecoverableKeyException){
+            unrecoverableKeyException.printStackTrace();
+        }
+
+
+        /*Private Key from the JCK file*/
+        RSAPrivateKey privateKey=(RSAPrivateKey)key;
+
+        // Create RSA-signer with the private key
+        JWSSigner signer = new RSASSASigner(privateKey);
+
+        // Prepare JWS object with simple string as payload
+        JWSObject jwsObject = new JWSObject(
+                new JWSHeader.Builder(JWSAlgorithm.RS256).keyID("123").build(),
+                new Payload("Ramesh"));
+
+        // Compute the RSA signature
+
+        try{
+            jwsObject.sign(signer);
+        }catch(JOSEException exception){
+            exception.printStackTrace();
+        }
+
+        String s = jwsObject.serialize();
+
+        System.out.println("Signed text :::" + s);
+
+
+        // To parse the JWS and verify it, e.g. on client-side
+        try {
+            jwsObject = JWSObject.parse(s);
+        }catch(ParseException parseEx){
+            parseEx.printStackTrace();
+        }
+
+        System.out.println("Input text ::"+ jwsObject.getPayload().toString());
+
+
+        /*Get the public key from JCK file*/
+        JWSVerifier verifier = new RSASSAVerifier((RSAPublicKey)cert.getPublicKey());
+
+
+        try {
+            /*Verify the content signature*/
+            if(jwsObject.verify(verifier))
+            {
+                System.out.println("Verified ::"+jwsObject.getPayload().toString());
+            }else{
+                System.out.println("Verification failed !!!");
+            }
+
+        }catch (JOSEException joseException){
+            joseException.printStackTrace();
+        }
+
+
+
     }
-    keyGenerator.initialize(1024);
-
-    KeyPair kp = keyGenerator.genKeyPair();
-    RSAPublicKey publicKey = (RSAPublicKey) kp.getPublic();
-    System.out.println("Public Key::"+ publicKey.toString() );
-
-    RSAPrivateKey privateKey = (RSAPrivateKey) kp.getPrivate();
-
-    System.out.println("Private Key::"+ publicKey.toString() );
-
-    // Create RSA-signer with the private key
-    JWSSigner signer = new RSASSASigner(privateKey);
-
-    // Prepare JWS object with simple string as payload
-    JWSObject jwsObject = new JWSObject(
-            new JWSHeader.Builder(JWSAlgorithm.RS256).keyID("123").build(),
-            new Payload("Ramesh"));
-
-// Compute the RSA signature
-
-    try{
-        jwsObject.sign(signer);
-    }catch(JOSEException exception){
-        exception.printStackTrace();
-    }
-
-
-    // To serialize to compact form, produces something like
-// eyJhbGciOiJSUzI1NiJ9.SW4gUlNBIHdlIHRydXN0IQ.IRMQENi4nJyp4er2L
-// mZq3ivwoAjqa1uUkSBKFIX7ATndFF5ivnt-m8uApHO4kfIFOrW7w2Ezmlg3Qd
-// maXlS9DhN0nUk_hGI3amEjkKd0BWYCB8vfUbUv0XGjQip78AI4z1PrFRNidm7
-// -jPDm5Iq0SZnjKjCNS5Q15fokXZc8u0A
-    String s = jwsObject.serialize();
-
-    System.out.print("Signed text :::" + s);
-
-
-// To parse the JWS and verify it, e.g. on client-side
-    try {
-        jwsObject = JWSObject.parse(s);
-    }catch(ParseException parseEx){
-        parseEx.printStackTrace();
-    }
-
-    System.out.println("Source ::"+ jwsObject.getPayload().toString());
-
-
-    JWSVerifier verifier = new RSASSAVerifier(publicKey);
-
-
-    try {
-        jwsObject.verify(verifier);
-    }catch (JOSEException joseException){
-        joseException.printStackTrace();
-    }
-
-    System.out.println("Verified:::"+jwsObject.getPayload().toString());
-
-}
 }
